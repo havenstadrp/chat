9f14198c2ddabeb691c907813ec1aa6936ab9221
Developer-Bear <collindudra@gmail.com>
2021-08-09 21:52:43 -0500

    import qb-core

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,17 +3,18 @@ game 'gta5'
 
 ui_page "html/index.html"
 
+shared_script '@qb-core/import.lua'
 
 client_scripts {
     'config.lua',
- 'client.lua',
+    'client.lua',
 }
 
 server_scripts {
     'config.lua',
- 'server.lua',
+    'server.lua',
 }
 
 files {
- 'html/*',
+    'html/*',
 }
